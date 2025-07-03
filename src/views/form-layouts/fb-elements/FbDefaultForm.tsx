4f9c630ddffaf8b939dab0a8e6fa4c3651fd84cf
RenzoAsFa <rascona@corsusa.com>
2025-07-03 11:53:27 -0500

    fix: update process client

--- src/views/form-layouts/fb-elements/FbDefaultForm.tsx
+++ src/views/form-layouts/fb-elements/FbDefaultForm.tsx
@@ -74,7 +74,7 @@ const FbDefaultForm = () => {
 
   // Estado para manejar la carga y errores
   const [loading, setLoading] = React.useState(false);
-  const [error, setError] = React.useState<string | null>(null);
+  const [/*error,*/ setError] = React.useState<string | null>(null);
   
   // Estados para los campos del formulario
   const [formData, setFormData] = React.useState<FormData>({
